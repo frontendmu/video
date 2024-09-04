d4375f3a12d5d1ef2da25feda2ccb964d7f9bd4c
GitHub Action <action@github.com>
2024-09-04 15:15:51 +0400

    add custom tailwind animation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ['./src/**/*.{ts,tsx}'],
+	theme: {
+		extend: {
+			animation: {
+        'spin-slow': 'spin 5s linear infinite',
+      }
+		},
+	},
+	plugins: [],
+};
